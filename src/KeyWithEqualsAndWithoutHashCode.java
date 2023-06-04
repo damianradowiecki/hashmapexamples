9cde78a15e8d63e4dce37b97faa24e921f6e948a
Damian Radowiecki <damianradowiecki@gmail.com>
2023-06-04 14:23:08 +0200

    initial commit

---
+++ src/KeyWithEqualsAndWithoutHashCode.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyWithEqualsAndWithoutHashCode {
+
+    public static void main(String[] args) {
+
+        Map<Key, String> map = new HashMap<>();
+        Key key1 = new Key(1);
+        Key key2 = new Key(1);
+
+        map.put(key1, "original value");
+        map.put(key2, "overridden value");
+
+        System.out.println(map.get(key1));
+        System.out.println(map.get(key2));
+        
+    }
+
+    static class Key {
+        private final int key;
+
+        public Key(int key) {
+            this.key = key;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return ((Key)obj).key == key;
+        }
+    }
+}
---
+++ src/KeyWithoutEqualsAndHashCode.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyWithoutEqualsAndHashCode {
+
+    public static void main(String[] args) {
+
+        Map<Key, String> map = new HashMap<>();
+        Key key1 = new Key(1);
+        Key key2 = new Key(1);
+
+        map.put(key1, "original value");
+        map.put(key2, "overridden value");
+
+        System.out.println(map.get(key1));
+        System.out.println(map.get(key2));
+
+    }
+
+    static class Key {
+        private final int key;
+
+        public Key(int key) {
+            this.key = key;
+        }
+    }
+}
